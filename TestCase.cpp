033eb28d9cca8f6985d9c97e3c689d413de467d8
dolevh123 <33119992+dolevh123@users.noreply.github.com>
2018-06-19 11:26:43 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,12 +1,12 @@
 #include "TestCase.hpp"
-TestCase::TestCase( string s, ostream& cerr): cerr(cerr.rdbuf()){
+TestCase::TestCase( string s, ostream& cerr): cerr(cerr.rdbuf()){//constructor
     Failed=0,Passed=0,Current=0;
     str=s;
 
     
 }
 
-void TestCase::print(){
+void TestCase::print(){// func to print result of failed , passedn and total.
     cerr << str + ": " +to_string(Failed)+" failed, " +to_string(Passed)+ " passed, "+to_string(Current)+ " total." << endl;
     cerr << "---" << endl;
 }
